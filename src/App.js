045751233352ab4e12e5fd5984c4bf8f0a8c8ff7
unknown <pratyushray010@gmail.com>
2024-09-28 16:22:15 +0530

    digitalclock committed

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import React,{Component} from 'react'
+import Digiclock from './Component/Digiclock';
+export default class App extends Component{
+  render(){
+    return(
+      <Digiclock/>
+    )
+  }
+}
---
+++ src/Component/Digiclock.jsx
@@ -0,0 +1,24 @@
+import React,{Component} from 'react'
+export default class Digiclock extends Component {
+    constructor() {
+      super();
+      this.state = {
+        time: '',
+      };
+    }
+    componentDidMount() {
+      this.IntervalId = setInterval(() => {
+        this.setState({ time: new Date().toLocaleTimeString() });
+      }, 1000);
+    }
+
+    render() {
+      return (
+        <>
+        <div className="clock-container ">
+          <h1 className="clock">{this.state.time}</h1>
+        </div>
+        </>
+        );
+    }
+}
